b146cc7ceef097715bcccc968d7aa54115c484d4
Simone Colaci <simone.colaci@nl.abnamro.com>
2024-02-01 14:04:37 +0100

    feat: added shortcuts to replace tmux

--- wezterm.lua
+++ wezterm.lua
@@ -19,12 +19,15 @@ else
   config.font_size = 11.0
 end
 
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
 config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = true
 config.bypass_mouse_reporting_modifiers = 'ALT'
 
 wezterm.font = wezterm.font 'CaskaydiaMono Nerd Font Mono'
+
+-- leader key
+config.leader = { key = ' ', mods = 'CTRL', timeout_milliseconds = 1000 }
 
 -- color scheme
 config.color_scheme = 'Ayu Dark (Gogh)'
@@ -33,26 +36,87 @@ config.colors = {
 }
 
 config.keys = {
-  -- Close the current pane
+  -- Close the current tab
   {
     key = 'w',
-    mods = 'ALT',
-    action = act.CloseCurrentPane { confirm = true },
+    mods = 'ALT|CMD',
+    action = act.CloseCurrentTab { confirm = true },
   },
 
   -- New tab
   {
     key = 't',
-    mods = 'ALT',
+    mods = 'ALT|CMD',
     action = act.SpawnTab 'CurrentPaneDomain',
   },
+
+  -- split pane horizontally
+  {
+    key = 'v',
+    mods = 'LEADER',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
+  },
+
+  -- split pane vertically
+  {
+    key = 's',
+    mods = 'LEADER',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
+  },
+
+  -- split pane vertically as small screen
+  {
+    key = 'o',
+    mods = 'LEADER',
+    action = wezterm.action.SplitPane {
+      direction = 'Down',
+      size = { Percent = 20 },
+    }
+  },
+
+  -- zoom current pane
+  {
+    key = 'z',
+    mods = 'LEADER',
+    action = wezterm.action.TogglePaneZoomState,
+  },
+
+  -- close current pane
+  {
+    key = 'w',
+    mods = 'LEADER',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+
+  -- list open tabs
+  {
+    key = 'e',
+    mods = 'LEADER',
+    action = wezterm.action.ShowTabNavigator
+  },
+
+
+  -- rotate panes
+  {
+    key = 'r',
+    mods = 'LEADER',
+    action = act.RotatePanes 'CounterClockwise',
+  },
+
+  -- set numbers of the panes
+  {
+    key = "p",
+    mods = "CTRL",
+    action = wezterm.action { PaneSelect = {} }
+  },
+
 }
 
 for i = 1, 8 do
   -- ALT + number to move to that position
   table.insert(config.keys, {
     key = tostring(i),
-    mods = 'ALT',
+    mods = 'CMD|ALT',
     action = act.ActivateTab(i - 1),
   })
 end
