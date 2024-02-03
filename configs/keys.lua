a3ce91424cb87f7a688e0cb2d87d006a28e6f7a3
Simone Colaci <sim.colaci@gmail.com>
2024-02-03 15:55:36 +0100

    feat: added command to rename current tab

--- configs/keys.lua
+++ configs/keys.lua
@@ -118,6 +118,22 @@ return function(config)
       action = act.ActivatePaneDirection 'Down',
     },
 
+    -- rename current tab
+    {
+      key = 'r',
+      mods = 'LEADER',
+      action = act.PromptInputLine {
+        description = 'Enter new name for tab',
+        action = wezterm.action_callback(function(window, pane, line)
+          -- line will be `nil` if they hit escape without entering anything
+          -- An empty string if they just hit enter
+          -- Or the actual line of text they wrote
+          if line then
+            window:active_tab():set_title(line)
+          end
+        end),
+      },
+    },
   }
 
   for i = 1, 8 do
